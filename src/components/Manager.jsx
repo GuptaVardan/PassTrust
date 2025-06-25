2d149e08de404f376e5b58cffa72973a78f9d2b2
GuptaVardan <guptavardan03@gmail.com>
2025-06-25 18:34:58 +0530

    third commit

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -5,9 +5,16 @@ const Manager = () => {
     <>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
 
-      <div className="container mx-auto">
-        <div className="text-white flex flex-col p-4">
-        
+      <div className="container mx-auto max-w-4xl bg-slate-50 my-10">
+        <div className= "flex flex-col p-4">
+          <h1 className='text-4xl text-center font-bold'>&lt;PassTrust/&gt;</h1>
+          <p className='text-xl text-center'>Your Personal Password Manager</p>
+          <input type="text" name="" id="" className='border-2 rounded-xl border-black w-full mx-auto my-5 p-1 text-black bg-white'/> 
+          <div className="flex justify-between gap-5">
+            <input type="text" name="" id="" className='border-2 rounded-xl border-black w-full my-2 mx-auto p-1 text-black bg-white'/>
+            <input type="text" name="" id="" className='border-2 rounded-xl border-black w-full my-2 mx-auto p-1 text-black bg-white'/>
+          </div>
+          <button className='border-2 rounded-xl border-green-500 my-2 mx-auto p-1 text-white bg-green-500 hover:font-bold hover:cursor-pointer w-1/2' >Add Password</button>
         </div>
       </div>
     </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const Nav = () => {
-  return (
-    <nav className='bg-purple-200 flex justify-between items-baseline px-4 h-18'>
-        <div className="logo font-bold text-xl">PassTrust</div>
-        <ul className=''>
-            <li className='flex gap-4'>
-                <a className='hover:font-bold' href="/">Home</a>
-                <a className='hover:font-bold' href="http://">About</a>
-                <a className='hover:font-bold' href="http://">Contact</a>
-            </li>
-        </ul>
-    </nav>
-  )
-}
+const Nav = () => (
+  <nav className="bg-purple-200">
+    <div className="container mx-auto flex justify-between items-center px-4">
+      <div className="logo font-bold text-xl">
+        &lt;PassTrust/&gt;
+      </div>
+      <ul className="flex gap-6">
+        <li><a className="hover:font-bold" href="/">Home</a></li>
+        <li><a className="hover:font-bold" href="/about">About</a></li>
+        <li><a className="hover:font-bold" href="/contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+)
 
 export default Nav
