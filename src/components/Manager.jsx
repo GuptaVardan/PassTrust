4c9882b7de842fe88904a356894c0bed8f7f7a78
GuptaVardan <guptavardan03@gmail.com>
2025-07-03 21:32:32 +0530

    fifth commit

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import { useRef } from 'react'
+import React, { use } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 const Manager = () => {
   const ref = useRef()
+  const [form,setForm] = useState({
+    website: '',
+    username: '',
+    password: ''
+  })
+  const [passwords, setPasswords] = useState([])
+
+  useEffect(() => {
+    const storedPasswords = JSON.parse(localStorage.getItem('passwords')) || []
+    setPasswords(storedPasswords)
+  }, [])
+
   const showPassword = () => {
     if (ref.current.src.includes("icons/eye.png")) {
       ref.current.src = "icons/eyecross.png"
@@ -11,6 +23,19 @@ const Manager = () => {
       ref.current.src = "icons/eye.png"
     }
   }
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value})
+  }
+
+  const savePassword = () => {
+    console.log(form);
+    setPasswords([...passwords, form]);
+    localStorage.setItem('passwords', JSON.stringify([...passwords, form]));
+    setForm({ website: '', username: '', password: '' });
+    console.log([...passwords, form]);
+  }
+
   return (
     <>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
@@ -19,17 +44,17 @@ const Manager = () => {
         <div className= "flex flex-col p-4">
           <h1 className='text-4xl text-center font-bold'>&lt;PassTrust/&gt;</h1>
           <p className='text-xl text-center'>Your Personal Password Manager</p>
-          <input placeholder='Enter the website URL' type="text" name="" id="" className='border-2 rounded-xl border-black w-full mx-auto my-5 p-2 text-black bg-white'/> 
+          <input value={form.website} onChange={handleChange} placeholder='Enter the website URL' type="text" name="website" id="website" className='border-2 rounded-xl border-black w-full mx-auto my-5 p-2 text-black bg-white'/> 
           <div className="flex justify-between gap-5">
-            <input placeholder='Enter the username' type="text" name="" id="" className='border-2 rounded-xl border-black w-full my-2 mx-auto p-2 text-black bg-white'/>
+            <input value={form.username} onChange={handleChange} placeholder='Enter the username' type="text" name="username" id="username" className='border-2 rounded-xl border-black w-full my-2 mx-auto p-2 text-black bg-white'/>
             <div className="relative w-full">
-              <input placeholder='Enter the password' type="text" name="" id="" className='border-2 rounded-xl border-black w-full my-2 mx-auto p-2 text-black bg-white'/>
+              <input value={form.password} onChange={handleChange} placeholder='Enter the password' type="text" name="password" id="password" className='border-2 rounded-xl border-black w-full my-2 mx-auto p-2 text-black bg-white'/>
               <span className="absolute right-0 top-0 hover:cursor-pointer" onClick={showPassword}>
                 <img ref={ref} src="icons/eye.png" alt="loading.." className="py-4.5 px-2" width={40}/>
               </span>
             </div>
           </div>
-          <button className='border-2 rounded-xl border-green-500 my-2 mx-auto p-2 text-white bg-green-500 hover:font-bold hover:cursor-pointer w-1/2' >Add Password</button>
+          <button onClick={savePassword} className='border-2 rounded-xl border-green-500 my-2 mx-auto p-2 text-white bg-green-500 hover:font-bold hover:cursor-pointer hover:bg-green-600 w-1/2 ' >Add Password</button>
         </div>
       </div>
     </>
